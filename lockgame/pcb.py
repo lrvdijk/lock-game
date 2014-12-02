f37bc406c426c42819d01f61ae4918f2912a48e7
Lucas van Dijk <info@return1.net>
2014-12-02 18:10:53 +0100

    Render SVG with PCB in a Gtk.DrawingArea

--- lockgame/pcb.py
+++ lockgame/pcb.py
@@ -1,11 +1,27 @@
-from gi.repository import Gtk, Gdk
-import cairo
+import collections
+
+from gi.repository import Gtk, Gdk, Rsvg
+import kdtree
+
+Pin = collections.namedtuple('Pin', 'node x y')
 
 class PCBWidget(Gtk.DrawingArea):
     def __init__(self, *args, **kwargs):
         Gtk.DrawingArea.__init__(self, *args, **kwargs)
 
-        self.components = {}
+        self.svg_handle = Rsvg.Handle.new_from_file(svg_file)
+        self.connect('draw', self.on_draw)
 
-    def on_draw(self):
-        pass
+    def on_draw(self, widget, ctx):
+
+        allocation = self.get_allocation()
+
+        self.modify_bg(Gtk.StateFlags.NORMAL, Gdk.Color((1/255)*65536,
+            (146/255)*65536, (62/255)*65536))
+
+        ratio_x = allocation.width / self.svg_handle.props.width
+        ratio_y = allocation.height / self.svg_handle.props.height
+
+        scale = min(ratio_x, ratio_y)
+        ctx.scale(scale, scale)
+        self.svg_handle.render_cairo(ctx)
