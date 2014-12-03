682a85bf9b3c44985aa9b7b7017ec6c5698eea5a
Lucas van Dijk <info@return1.net>
2014-12-04 00:24:26 +0100

    Initialize thread support for glib

--- lock-game.py
+++ lock-game.py
@@ -1,8 +1,10 @@
-from gi.repository import Gtk
+from gi.repository import Gtk, GObject
 
 from lockgame.window import MainWindow
 
 if __name__ == '__main__':
+    GObject.threads_init()
+
     window = MainWindow()
     window.connect('delete-event', Gtk.main_quit)
     window.show_all()
